25632eb747ff8133159bd1e4b183915f516cf1df
Nicoleordonezc <nicoloe24@gmail.com>
2025-08-26 21:19:28 -0500

    feat: :lipstick: Created models for each collection

---
+++ src/models/content.js
@@ -0,0 +1,11 @@
+export class Content {
+    constructor(title, description, year, category, approved, type) {
+
+        this.title = title;
+        this.description = description;
+        this.year = year;
+        this.category = category;
+        this.approved = approved;
+        this.type = type
+    }
+}
---
+++ src/models/users.js
@@ -0,0 +1,10 @@
+export class Users {
+    #password = 0
+    constructor(name, email, rol) {
+
+        this.name = name;
+        this.email = email;
+        this.rol = rol;
+        this.#password = password
+    }
+}
